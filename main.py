238cebe35de220b4fb1db9f842bf1f081142ddae
silvergun8291 <sv5506829sv@gmail.com>
2023-02-08 17:31:37 +0900

    2023-02-08

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-def main() -> list[int]:
-    num: list[int] = list(map(int, input().split()))
-    return num
+import pymysql
 
+conn = pymysql.connect(host='127.0.0.1', user='root', password='toor', db='py_db', charset='utf8')
+cur = conn.cursor()
 
-if __name__ == "__main__":
-    result: list[int] = main()
-    print(*result)
+cur.execute("SELECT * FROM userTable")
 
-    
+print("ID     이름    이메일          출생연도")
+print("---------------------------------------")
+
+while(True):
+    row = cur.fetchone()
+    if row == None:
+        break
+    id = row[0]
+    name = row[1]
+    email = row[2]
+    birth = row[3]
+    print(f'{id:5s} {name:5s} {email:17s} {birth:d}')
+
+conn.commit()
+conn.close()
